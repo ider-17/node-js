f62d164d499f1adf0d5e53d67689336efd0f93a8
24LP6042 24LP6042 <24LP6042@G303-CC0142.local>
2025-01-14 12:37:03 +0800

    2025 01 14

--- index.js
+++ index.js
@@ -1,17 +1,78 @@
 import chalk from 'chalk';
 
-console.log(chalk.blue('Hello world!'));
-console.log(chalk.bgGreen('haha'));
 
-// const fs = require('node:fs');
-import fs from 'node:fs'
-const content = 'Some content!';
+// console.log(chalk.blue('Hello world!'));
+// console.log(chalk.bgGreen('haha'));
 
-fs.writeFile('./test.txt', content, err => {
-    if (err) {
-        console.error(err)
-    } else {
-        // file written successfully
-    }
-});
-console.log(fs.readFile(test.txt));
+// // const fs = require('node:fs');
+import fs from 'fs'
+// import path from 'node:path';
+// const content = 'Some content!';
+
+// fs.writeFile('./test.txt', content, err => {
+//     if (err) {
+//         console.error(err)
+//     } else {
+//         // file written successfully
+//     }
+// });
+// console.log(fs.readFile(test.txt));
+
+
+
+// const filenames = fs.readdirSync('Users/24LP6042/Desktop/ider/node-js');
+// for (let i = 0; i < filenames.length; i++) {
+//     console.log(filenames[i]);
+// }
+
+
+
+// fs.writeFile('./text.txt', 'Hello world', (err) => {
+//     if (err) {
+//         console.error(err)
+//     } else {
+//         console.log('no');
+        
+//     }
+// });
+
+// console.log(fs.readFile(text.txt));
+
+
+
+
+// const createdFile = fs.writeFileSync('./test.txt', 'Hello world');
+// console.log(createdFile);
+
+// let unshsanFile = fs.readFileSync('./test.txt', 'utf-8');
+
+// fs.appendFileSync('./test.txt', 'Bye')
+//     console.log('Updated!');
+
+// const unshih = fs.readFileSync('./test.txt', 'utf-8')
+// console.log(unshih);
+
+// let a = fs.writeFileSync('./test.txt', 'Byetai')
+//     console.log(a);
+
+
+// const b = fs.readFileSync('./students.json', 'utf-8') 
+// console.log(b);
+
+
+// let myObject = JSON.parse(b);
+
+// let newData = {
+//     "name": "ider",
+//     "sit": "1C"
+// }
+
+// myObject.push(newData);
+
+// newData = JSON.stringify(myObject);
+// fs.writeFile('students.json', newData, err => {
+//     if (err) throw err;
+    
+//     console.log("New data edded");
+    
+// })
