6457dab3b37bf665fc9410aa711a78e3599e9f90
24LP6042 24LP6042 <24LP6042@G303-CC0142.local>
2025-01-15 13:17:57 +0800

    2025 01 15

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import chalk from 'chalk';
 
 // // const fs = require('node:fs');
 import fs from 'fs'
+import { queryObjects } from 'v8';
 // import path from 'node:path';
 // const content = 'Some content!';
 
@@ -56,6 +57,9 @@ import fs from 'fs'
 //     console.log(a);
 
 
+
+
+
 // const b = fs.readFileSync('./students.json', 'utf-8') 
 // console.log(b);
 
@@ -70,9 +74,99 @@ import fs from 'fs'
 // myObject.push(newData);
 
 // newData = JSON.stringify(myObject);
+
 // fs.writeFile('students.json', newData, err => {
 //     if (err) throw err;
     
 //     console.log("New data edded");
     
 // })
+
+
+
+
+// const text = '{"name":"John", "birth":"1986-12-14", "city":"New York"}';
+// const obj = JSON.parse(text, function (key, value) {
+//   if (key == "birth") {
+//     return new Date(value);
+//   } else {
+//     return value;
+//   }
+// });
+
+// console.log(obj.name + "," + obj.birth);
+
+
+
+
+// let myObj = {
+// 	'prop_1': {
+// 		'prop_11': 'value_11',
+// 		'prop_12': 'value_12'
+// 	}
+// };
+
+// function removeJsonAttr() {
+// 	let key = "prop_12";
+// 	delete myObj.prop_1[key];
+	
+// 	console.log(JSON.stringify(myObj));
+// }
+
+// removeJsonAttr();
+
+
+
+
+
+// const a = fs.readFileSync('./students.json', 'utf-8');
+
+// const b = JSON.parse(a)
+
+// let c = b.filter(function (e) {
+//     return e.name !== 'Jack';
+// });
+
+// console.log(c);
+
+
+
+
+
+// var a = [{name:'tc_001'}, {name:'tc_002'}, {name:'tc_003'}]
+
+// var b = a.filter(function (e) {
+//     return e.name === 'tc_001';
+// });
+
+// b.splice(0,1);
+
+// console.log(a);
+// console.log(b);
+
+
+
+
+
+// create
+
+function createStudent() {
+    const create = fs.writeFileSync('student.json', '[{"name": "Ider", \n"seat": "1C", \n"id": "1"}]')
+}
+
+createStudent()
+
+// read & add
+
+function readStudent() {
+    const read = fs.readFileSync('student.json', 'utf-8');
+    const data = JSON.parse(read)
+//    return data
+   data.push({"name": "Jack"})
+fs.writeFileSync('student.json', JSON.stringify(data))
+   
+}
+
+readStudent();
+
+
